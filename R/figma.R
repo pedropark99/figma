f6bcf9959c56230c6656896da6448716846e79b0
pedropark99 <pedropark99@gmail.com>
2022-10-25 07:00:54 -0300

    Refactor endpoint URL searching scheme

--- R/figma.R
+++ R/figma.R
@@ -4,27 +4,14 @@
 #'
 #'
 #' You can easily get the key of your Figma File from the URL of this file.
-#' In other words, when you visit this file in your web browser, the URL
-#' will probably look something like this:
-#'
-#' \code{https://www.figma.com/file/hch8YlkIrYbU3raDzjPvCz/Untitled}
-#'
-#' In the above example, the string \code{hch8YlkIrYbU3raDzjPvCz} is the
-#' key to the file. So, every URL to a Figma File comes in the following format:
-#'
-#' \code{https://www.figma.com/file/file_key/file_title}
 #'
 #' @param file_key A string with the key of the Figma File you want to get;
 #' @param token A string with your personal Figma token to authenticate in the API;
 #'
 #' @returns A Figma Document object (i.e. a object of class `figma_document`);
 get_figma_file <- function(file_key, token){
-  url <- get_files_url()
-  url <- paste0(
-    url,
-    file_key,
-    collapse = ""
-  )
+  url <- get_endpoint_url(endpoint = "files")
+  url <- build_request_url(url, path = file_key)
   header <- httr::add_headers(
     "X-Figma-Token" = token
   )
--- R/url.R
+++ R/url.R
@@ -4,20 +4,58 @@ api_endpoints <- list(
   files = "/v1/files/"
 )
 
+implemented_endpoints <- names(api_endpoints)
 
-concat_urls <- function(...){
-  components <- list(...)
-  components <- as.character(components)
-  url <- paste(components, collapse = "/")
+
+build_request_url <- function(base_url, path = NULL, query_string = NULL){
+  url <- base_url
+  if (is_not_null(path) && is.character(path)) {
+    url <- add_paths_to_url(url, path)
+  }
+  if (is_not_null(query_string) && is.character(query_string)) {
+    url <- add_query_string_to_url(url, query_string)
+  }
   return(url)
 }
 
 
-get_files_url <- function(){
-  url <- paste0(
-    api_base_url, api_endpoints$files,
-    collapse = ""
-  )
+is_not_null <- function(x){
+  !is.null(x) && !is.na(x)
+}
 
+#' Get the URL to each of the implemented endpoints of Figma API
+#'
+#' @param endpoint a string with the name of one of
+get_endpoint_url <- function(endpoint){
+  check_endpoint(endpoint)
+  endpoint_url <- api_endpoints[[endpoint]]
+  url <- paste0(api_base_url, endpoint_url)
   return(url)
+}
+
+
+check_endpoint <- function(endpoint){
+  msg_components <- c(
+    "The given endpoint is %s. ",
+    "However, the `endpoint` argument should be a single string ",
+    "with the name of the endpoint you want to use. ",
+    "Check `print(figma::implemented_endpoints)` to see the available values ",
+    "for `endpoint` argument."
+  )
+  msg_format <- paste(msg_components, collapse = "")
+
+  if (is.null(endpoint) || is.na(endpoint)) {
+    msg <- sprintf(msg_format, "a NULL or NA value")
+    stop(msg)
+  }
+
+  if (length(endpoint) != 1) {
+    msg <- sprintf(msg_format, "a vector of strings")
+    stop(msg)
+  }
+
+  if (!endpoint %in% implemented_endpoints) {
+    msg <- sprintf(msg_format, endpoint)
+    stop(msg)
+  }
 }
